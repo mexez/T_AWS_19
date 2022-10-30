dac5f9ff5ab669eb448b1ff94db4018bb0ec91f4
mexez <eezeali@gmail.com>
2022-10-30 07:18:51 -0400

    up

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "= 3.74.2"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "= 3.74.2"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = var.region
